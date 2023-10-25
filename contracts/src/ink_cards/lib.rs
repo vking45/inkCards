a86a432b34d5c26f368ba8e4978b65f4ec9e7873
mystical-prog <jdgt.vd.0405@gmail.com>
2023-10-25 16:14:45 +0530

    barebones added

--- contracts/src/ink_cards/lib.rs
+++ contracts/src/ink_cards/lib.rs
@@ -1,3 +1,47 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 pub mod traits;
+
+#[openbrush::implementation(PSP34, Ownable, PSP34Metadata)]
+#[openbrush::contract]
+pub mod ink_cards {
+    use crate::traits::card::*;
+    use openbrush::{
+        modifiers,
+        storage::Mapping,
+        traits::{Storage, String},
+    };
+
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct PoolContract {
+        #[storage_field]
+        psp34: psp34::Data,
+        #[storage_field]
+        ownable: ownable::Data,
+        #[storage_field]
+        metadata: metadata::Data,
+
+        pool_name: String,
+        cards_mapping: Mapping<Id, CardInfo>,
+        last_card_id: Id,
+    }
+
+    impl PoolContract {
+        #[ink(constructor, payable)]
+        pub fn new(pool_name: String) -> Self {
+            let mut instance = Self::default();
+            instance.pool_name = pool_name;
+            instance.last_card_id = Id::U128(1);
+            metadata::Internal::_set_attribute(
+                &mut instance,
+                Id::U8(1u8),
+                String::from("InkCards NFT"),
+                String::from("ICNFTS"),
+            );
+            ownable::Internal::_init_with_owner(&mut instance, Self::env().caller());
+
+            instance
+        }
+    }
+}
--- contracts/src/ink_cards/traits/card.rs
+++ contracts/src/ink_cards/traits/card.rs
@@ -12,6 +12,7 @@ use ink::storage::traits::StorageLayout;
 #[derive(Debug, Clone, scale::Encode, scale::Decode)]
 #[cfg_attr(feature = "std", derive(StorageLayout, scale_info::TypeInfo))]
 pub struct CardInfo {
+    pub spent_amount: Balance,
     pub spend_limit: Balance,
     pub expiration: Timestamp,
 }
@@ -19,6 +20,7 @@ pub struct CardInfo {
 impl Default for CardInfo {
     fn default() -> Self {
         Self {
+            spent_amount: Balance::default(),
             spend_limit: Balance::default(),
             expiration: Timestamp::default(),
         }
@@ -32,4 +34,10 @@ pub type CardRef = dyn Card + PSP34 + PSP34Metadata + Ownable;
 pub trait Card: PSP34 + PSP34Metadata + Ownable {
     #[ink(message)]
     fn issue_card(&mut self, card_info: CardInfo) -> Result<(), PSP34Error>;
+
+    #[ink(message)]
+    fn spend_from_card(&mut self, card_id: Id) -> Result<(), PSP34Error>;
+
+    #[ink(message)]
+    fn get_card_info(&self, card_id: Id) -> Result<CardInfo, PSP34Error>;
 }
