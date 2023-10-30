1d2e0bc9ad91e50f1a2b5c4a954d7836d96780ef
vking45 <d.vraj.4523@gmail.com>
2023-10-31 01:10:32 +0530

    fix attempt 2

--- frontend/src/pages/_index.tsx
+++ frontend/src/pages/_index.tsx
@@ -3,7 +3,6 @@ import { HomeTopBar } from '@/components/home/HomeTopBar'
 import { CenterBody } from '@/components/layout/CenterBody'
 import { ChainInfo } from '@/components/web3/ChainInfo'
 import { ConnectButton } from '@/components/web3/ConnectButton'
-import { GreeterContractInteractions } from '@/components/web3/GreeterContractInteractions'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useInkathon } from '@scio-labs/use-inkathon'
 import type { NextPage } from 'next'
@@ -34,9 +33,6 @@ const HomePage: NextPage = () => {
         <div tw="mt-10 flex w-full flex-wrap items-start justify-center gap-4">
           {/* Chain Metadata Information */}
           <ChainInfo />
-
-          {/* Greeter Read/Write Contract Interactions */}
-          <GreeterContractInteractions />
         </div>
       </CenterBody>
     </>
